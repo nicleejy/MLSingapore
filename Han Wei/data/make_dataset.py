8181d89b9f1ad60c18f6442859bb52e7b151a61f
rexong <rexonghanwei200035@gmail.com>
2024-04-04 15:30:27 +0800

    add method to data

--- Han Wei/data/make_dataset.py
+++ Han Wei/data/make_dataset.py
@@ -2,12 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 import cv2
+from sklearn.model_selection import train_test_split
 
 # Get the current directory of this file
 CURR_DIR = os.path.dirname(__file__)
 
 # Specify the path to the image data directory [Change to your own path]
-DATA_DIR = "../../data/imagery/realsense_overhead"
+IMAGE_DATA_DIR = "../../data/imagery/realsense_overhead"
+CSV_DATA_DIR = "../../data/metadata/nutrition5k_dataset_metadata_dish_metadata_cafe1.csv"
 
 # [HARDCODED VALUE] - current image size, without augmentation 
 IMAGE_SIZE = (480, 640, 3)
@@ -29,8 +31,8 @@ Returns a dictionary, where
 '''
 def get_dishes_rgb_image():
     processed_images = {}
-    for directory in os.listdir(DATA_DIR):
-        directory_path = os.path.join(DATA_DIR, directory)
+    for directory in os.listdir(IMAGE_DATA_DIR):
+        directory_path = os.path.join(IMAGE_DATA_DIR, directory)
 
         if os.path.isdir(directory_path):
             rgb_image_path = os.path.join(directory_path, "rgb.png")
@@ -60,13 +62,93 @@ def convert_flatten_image_to_original_size(flatten_image):
     image = flatten_image.reshape(-1,1)
     return image.reshape(IMAGE_SIZE)
 
-if __name__ == "__main__":
-    test_dish = "dish_1573234760"
+'''
+Given an image dictionary, where
+    key is dish_id
+    value is RGB values
+Return DataFrame with 
+    first column - dish_id
+    Remaining columns - calories, mass, fat, carb, protein
+Where only dishes in image dictionary are kept in the dataframe
+
+Note:
+For the CSV, there exist rows that are formatted wrongly.
+For these wrongly formatted rows, the following script will just skip the rows.
+As such, there is a possibility where 
+    length of dish_images > length of data returned
+'''
+def get_dish_nutrients(dish_images):
+    columns = ['dish_id', 'calories', 'mass', 'fat', 'carb', 'protein']
+    dishes = dish_images.keys()
+    raw_data = pd.read_csv(CSV_DATA_DIR, on_bad_lines='skip', header=None, usecols=range(6))
+    data = raw_data[raw_data[0].isin(dishes)]
+    data.columns = columns
+    return data
+
+'''
+Given images and nutrients dataframe,
+Return a merged dataframe on dish_id
+'''
+def merge_images_nutrient(images_df, nutrients_df):
+    nutrients_df = nutrients_df.merge(images_df, how='left', on='dish_id')
+    return nutrients_df
+
+'''
+Given a DataFrame, with
+    dish_id, mass, calories, fat, protein and carb columns
+    image column as flatten image
+Split the dataframe into feature and labels, where
+    feature as the original shape of image
+    label as the 5 nutrient values in the dataframe
+Return a tuple (feature, label)
+'''
+def feature_label_split(data):
+    original_feature = []
+    features = data["image"].values
+    for feature in features:
+        feature = feature.reshape(-1,1)
+        feature = feature.reshape(IMAGE_SIZE)
+        original_feature.append(np.array(feature))
+    labels = data[['mass', 'calories', 'fat', 'protein', 'carb']].values
+
+    return np.array(original_feature), labels
+
+'''
+Given 
+    train_test_ratio - ratio of data to use as train
+    train_validate_ratio - ratio of data to use as train
+    random_state - random seed
+The function works through a pipeline
+    1. Get images from DATA_DIR
+    2. Convert the images into a Dataframe
+    3. Get the nutrients values as Dataframe
+    4. Merge the images and nutrients Dataframes
+    5. Perform train test split
+    6. Perform train validation split
+    7. Return train, validate, test feature and labels
+'''
+def data_pipe(train_test_ratio, train_validate_ratio, random_state=0):
     images = get_dishes_rgb_image()
-    print(images[test_dish].shape)
-    print("Number of dishes:", len(images))
     images_df = convert_dishes_rgb_image_to_dataframe(images)
-    print(images_df)
-    print(images_df.loc[images_df['dish_id'] == test_dish])
+    nutrients_df = get_dish_nutrients(images)
+    data = merge_images_nutrient(images_df, nutrients_df)
+
+    train, test = train_test_split(data, train_size=train_test_ratio, random_state=random_state)
+    train, val = train_test_split(train, train_size=train_validate_ratio, random_state=random_state)
+
+    return feature_label_split(train), feature_label_split(val), feature_label_split(test) 
+
+
+if __name__ == "__main__":
+    train, val, test = data_pipe(0.8, 0.9)
+    X_train, y_train = train
+    X_val, y_val = val
+    X_test, y_test = test
+    print("X_train shape: ", X_train.shape)
+    print("y_train shape: ", y_train.shape)
+    print("X_validate shape: ", X_val.shape)
+    print("y_validate shape: ", y_val.shape)
+    print("X_test shape: ", X_test.shape)
+    print("y_test shape: ", y_test.shape)
 
 
